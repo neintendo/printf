64d6251943233e6aa477578c1a1a36218168f279
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-09-17 15:46:58 +0200

    printf re-upload

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int handle_char(char c, int len);
+int handle_string(char *s, int len);
+int handle_integer(int i, int len);
+int handle_percent(int len);
+
+#endif
